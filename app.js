eec3bf32403dd514a029a5966cb6910530a7c520
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2020-10-31 21:08:12 -0300

    agrego dialertask

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 // CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://cleverideas.netlify.app');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
--- controllers/project.js
+++ controllers/project.js
@@ -1,7 +1,9 @@
 'use strict'
 
 var Project = require('../models/project');
+var axios = require('axios');
 var fs = require('fs');
+var qs = require('qs');
 
 let controller = {
     home: function (req, res) {
@@ -85,7 +87,7 @@ let controller = {
             var filepath = req.files.image.path;
             var filesplit = filepath.split("\\");
             var filename = filesplit[1];
-            var extsplit = filename.split('\.');
+            var extsplit = fileName.split('\.');
             var fileext = extsplit[1];
 
             if (fileext == "png" || fileext == "jpg" || fileext == "jpeg" || fileext == "gif") {
@@ -109,11 +111,104 @@ let controller = {
                 message: "no se pudo subir la imagen"
             })
         }
+    },
+
+    dialerCall: async (req, res) => {
+        token = atob(req.params.token);
+
+        if (token.split(':')[0] === process.env.USERTOKEN && token.split(':')[1] === process.env.PASSTOKEN) {
+            let tokenito = await Login();
+            let resp = await Dial(tokenito, req.body.info, req.body.phone, req.body.dialer);
+            console.log(resp);
+            await CloseSession(tokenito);
+            return res.status(200).send({ result: resp != false ? "OK" : "BAD" });
+
+        } else {
+            return res.status(400).send({
+                result: "check your token"
+            });
+        }
+
+
     }
+
+
 
 
 };
 
+async function Login() {
+
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("user", process.env.USERTOKEN);
+    urlencoded.append("password", process.env.PASSTOKEN);
+
+    let respi = await axios.post(`https://${ENVUC}.ucontactcloud.com/Integra/resources/auth/UserLogin`,
+        urlencoded,
+        {
+            headers:
+            {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Access-Control-Allow-Origin": "*",
+                "Accept": "*/*"
+            }
+        });
+
+    return respi ? respi.data[2] : false;
 
 
+}
+
+async function Dial(token, info, phone, dialer) {
+
+    let par = new URLSearchParams();
+    par.append("call", `{"calldate" : null, "campaign" : "${dialer}","destination": "${phone}","alternatives": "","agent" : "","data": "phone=${phone}:name=${info.name}:country=${info.country}:state=${info.state}:email=${info.email}:field1=How did you know? ${info.howdid}:field2=Want test? ${info.didyou}","source":"source","bulk" : false,"automatic" : true }`);
+
+    let dialersent = await axios.post('https://democlever.ucontactcloud.com/Integra/resources/Dialers/DialerTask', par,
+        {
+            headers:
+            {
+                "Authorization": `Basic ${token}`,
+                "Content-Type": "application/x-www-form-urlencoded ; charset=UTF-8",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*",
+                'Access-Control-Allow-Credentials': '*',
+                "Accept": "*/*",
+                "cache-control": "no-cache"
+            }
+        }
+    ).then((response) => { console.log(response) })
+        .catch((error) => { console.log(error) })
+
+    console.log(dialersent.data);
+}
+
+async function CloseSession(token) {
+
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    myHeaders.append("Authorization", "Basic " + token);
+
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("user", process.env.USERTOKEN);
+    urlencoded.append("token", token);
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+
+    fetch(`https://${process.env.ENVUC}.ucontactcloud.com/Integra/resources/auth/EndSession`, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            return (result === 1 ? true : false);
+        })
+        .catch(error => console.log('error', error));
+
+
+
+}
 module.exports = controller;
